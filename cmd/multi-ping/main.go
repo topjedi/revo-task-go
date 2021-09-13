f0d25bdfd9ec36297e8220a1acc4d42e37126589
Pavel Iovlev <p.iovlev@agima.ru>
2021-09-13 07:48:38 +0300

    main | v1 refactor

--- cmd/multi-ping/main.go
+++ cmd/multi-ping/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"revo-task-go/cmd/multi-ping/app"
@@ -9,11 +8,5 @@ import (
 
 func main() {
 	http.HandleFunc("/sites/", app.Handler)
-	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":80", nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello world")
 }
